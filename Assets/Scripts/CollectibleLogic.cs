8c8d594df9ab964a4162f45bd343dcc453b48065
sumairijazhashmi <sumairijazhashmi@gmail.com>
2022-09-10 21:04:54 +0500

    NPCs placed and they fall after collision w bakri

--- Assets/Scripts/CollectibleLogic.cs
+++ Assets/Scripts/CollectibleLogic.cs
@@ -4,12 +4,35 @@ using UnityEngine;
 
 public class CollectibleLogic : MonoBehaviour
 {
+    Animator animator;
+    public bool setFollow = false;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        //Debug.Log("here");
+    //        animator.SetBool("playerHit", true);
+    //        //Destroy(gameObject);
+    //    }
+    //}
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
             //Debug.Log("here");
-            Destroy(gameObject);
+            // follow player
+            //setFollow = true;
+            animator.SetBool("playerHit", true);
+
+            //Destroy(gameObject);
         }
     }
+
 }
