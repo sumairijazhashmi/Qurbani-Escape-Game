34caaf8c24700cb03ade055f2df500da2dcb5feb
sumairijazhashmi <sumairijazhashmi@gmail.com>
2022-09-11 14:17:22 +0500

    jump animation for npc, but it dont always work

--- Assets/Scripts/NPCLogic.cs
+++ Assets/Scripts/NPCLogic.cs
@@ -9,6 +9,8 @@ public class NPCLogic : MonoBehaviour
     //public bool setFollow = false;
     public Player player;
     public EnemyFollow enemyFollow;
+    public GameObject go; // obstacle
+    RaycastHit hit;
 
     private void Start()
     {
@@ -19,18 +21,43 @@ public class NPCLogic : MonoBehaviour
 
     private void Update()
     {
+        animator.SetBool("obstacle", false);
+
+
         // try to grab bakri
-        if(Mathf.Abs(Vector3.Distance(transform.position, player.transform.position)) <= 2.0f)
+        if (Mathf.Abs(Vector3.Distance(transform.position, player.transform.position)) <= 2.0f)
         {
             animator.SetBool("playerCross", true);
         }
-        
+
         // run after bakri
         if ((Mathf.Abs(Vector3.Distance(transform.position, player.transform.position)) <= 1.5f))
         {
-            
+
             enemyFollow.attach2npc = true;
         }
+
+        hit = new RaycastHit();
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 3f) || Physics.Raycast(transform.position, transform.TransformDirection(Vector3.back), out hit, 3f) || Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hit, 3f) || Physics.Raycast(transform.position, transform.TransformDirection(Vector3.right), out hit, 3f) || Physics.Raycast(transform.position, transform.TransformDirection(Vector3.up), out hit, 3f) || Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 3f))
+        {
+
+            print("here");
+
+            if (hit.collider.tag == "obstacle")
+            {
+                animator.SetBool("obstacle", true);
+
+            }
+        }
+
+        // jump if obj = obstacle
+        //if (go.tag == "obstacle" && (Mathf.Abs(Vector3.Distance(transform.position, go.transform.position)) <= 1.5f))
+        //{
+        //    print("here");
+        //    animator.SetBool("obstacle", true);
+        //}
+
+
 
     }
 
