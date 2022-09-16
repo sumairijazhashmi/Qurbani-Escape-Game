defaf0f33b7d365b10cccf286dbff433c42f5b83
Sumair Ijaz Hashmi <85024854+sumairijazhashmi@users.noreply.github.com>
2022-09-16 20:38:50 +0500

    changed swipe coe a bit

--- Assets/Scripts/SwipeControls.cs
+++ Assets/Scripts/SwipeControls.cs
@@ -2,96 +2,59 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-// https://gist.github.com/Fonserbc/ca6bf80b69914740b12da41c14023574
-
-/*
- * Swipe Input script for Unity by @fonserbc, free to use wherever
- *
- * Attack to a gameObject, check the static booleans to check if a swipe has been detected this frame
- * Eg: if (SwipeInput.swipedRight) ...
- *
- * 
- */
+// reference: https://gist.github.com/Fonserbc/ca6bf80b69914740b12da41c14023574
 
 public class SwipeControls : MonoBehaviour
 {
 
-	// If the touch is longer than MAX_SWIPE_TIME, we dont consider it a swipe
-	public const float MAX_SWIPE_TIME = 0.5f;
+	private float MIN_SWIPE_DISTANCE = (20/100) * Screen.width;
 
-	// Factor of the screen width that we consider a swipe
-	// 0.17 works well for portrait mode 16:9 phone
-	public const float MIN_SWIPE_DISTANCE = 0.17f;
+	public bool MoveRight = false;
+	public bool MoveLeft = false;
+	public bool Jump = false;
 
-	public bool swipedRight = false;
-	public bool swipedLeft = false;
-	public bool swipedUp = false;
-	public bool swipedDown = false;
-
-
-	public bool debugWithArrowKeys = true;
-
-	Vector2 startPos;
-	float startTime;
+	Vector2 startPosition;
 
 	public void Update()
 	{
-		swipedRight = false;
-		swipedLeft = false;
-		swipedUp = false;
-		swipedDown = false;
+		MoveRight = MoveLeft = Jump = false;
 
-		if (Input.touches.Length > 0)
+		if (Input.touchCount > 0)
 		{
-			Touch t = Input.GetTouch(0);
-			if (t.phase == TouchPhase.Began)
+			Touch currentTouch = Input.GetTouch(0);
+			if (currentTouch.phase == TouchPhase.Began)
 			{
-				startPos = new Vector2(t.position.x / (float)Screen.width, t.position.y / (float)Screen.width);
-				startTime = Time.time;
+				startPosition = new Vector2(currentTouch.position.x / (float)Screen.width, currentTouch.position.y / (float)Screen.width);
 			}
-			if (t.phase == TouchPhase.Ended)
+			if (currentTouch.phase == TouchPhase.Ended)
 			{
-				if (Time.time - startTime > MAX_SWIPE_TIME) // press too long
+
+				Vector2 endPosition = new Vector2(currentTouch.position.x / (float)Screen.width, currentTouch.position.y / (float)Screen.width);
+
+				Vector2 swipeDirection = new Vector2(endPosition.x - startPosition.x, endPosition.y - startPosition.y);
+
+				if (swipeDirection.magnitude < MIN_SWIPE_DISTANCE) // Too short swipe
 					return;
 
-				Vector2 endPos = new Vector2(t.position.x / (float)Screen.width, t.position.y / (float)Screen.width);
-
-				Vector2 swipe = new Vector2(endPos.x - startPos.x, endPos.y - startPos.y);
-
-				if (swipe.magnitude < MIN_SWIPE_DISTANCE) // Too short swipe
-					return;
-
-				if (Mathf.Abs(swipe.x) > Mathf.Abs(swipe.y))
-				{ // Horizontal swipe
-					if (swipe.x > 0)
+				if (Mathf.Abs(swipeDirection.x) > Mathf.Abs(swipeDirection.y))
+				{ 
+					if (swipeDirection.x > 0)
 					{
-						swipedRight = true;
+						MoveRight = true;
 					}
 					else
 					{
-						swipedLeft = true;
+						MoveLeft = true;
 					}
 				}
 				else
-				{ // Vertical swipe
-					if (swipe.y > 0)
+				{ 
+					if (swipeDirection.y > 0)
 					{
-						swipedUp = true;
-					}
-					else
-					{
-						swipedDown = true;
+						Jump = true;
 					}
 				}
 			}
-		}
-
-		if (debugWithArrowKeys)
-		{
-			swipedDown = swipedDown || Input.GetKeyDown(KeyCode.DownArrow);
-			swipedUp = swipedUp || Input.GetKeyDown(KeyCode.UpArrow);
-			swipedRight = swipedRight || Input.GetKeyDown(KeyCode.RightArrow);
-			swipedLeft = swipedLeft || Input.GetKeyDown(KeyCode.LeftArrow);
 		}
 	}
 }
