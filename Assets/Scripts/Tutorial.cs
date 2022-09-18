ea662de221afd31dfa293dc3a660f6ebdbbe1634
Supsicious Bird <arslank3377@gmail.com>
2022-09-18 12:22:26 +0000

    IDK what this is

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tutorial : MonoBehaviour
+{
+    [SerializeField] TMPro.TMP_Text swipeRight;
+    [SerializeField] TMPro.TMP_Text swipeLeft;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
